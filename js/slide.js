9eebbc0235e7f01d66dd076265dfe2b85afcce6b
handha0327 <112833838+handha0327@users.noreply.github.com>
2022-10-11 11:24:15 +0900

    221011_1

--- js/slide.js
+++ js/slide.js
@@ -2,7 +2,9 @@ $(function(){
   $(".lazy").slick({
     lazyLoad: 'ondemand', // ondemand progressive anticipated
     infinite: true,
-    arrows : false,
-    dots : true
+    arrows : true,
+    dots : true,
+    autoplay: true,
+    autoplaySpeed : 5000
   });
 })
